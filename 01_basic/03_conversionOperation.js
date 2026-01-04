75efdb7ae1bcdaa9e7158a4941fa8262d8ba19fa
Nadim Akrma <nadimakram759@gmail.com>
2026-01-04 14:59:41 +0000

    Conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+//let score = undefined
+// let score = true
+let score = "nadim"
+
+// console.log(typeof score);
+// console.log(typeof (score) );
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 / false => 0
+//
+
+// let isLoggedIn = 1;
+// let isLoggedIn = ""
+let isLoggedIn = "nadim"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "nadim" => true
+
+
+let sumNumber = 33
+let stringNumber = String(sumNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
