9916bc271b850468808e3a0522f10be8c8242aee
Nadim Akrma <nadimakram759@gmail.com>
2026-01-04 05:51:32 +0000

    Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144534
+let accountEmail = "nadimakram759@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+// accountId = 66 // not change
+accountEmail = "goutam602@gmail.com"
+accountPassword = "54321"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
