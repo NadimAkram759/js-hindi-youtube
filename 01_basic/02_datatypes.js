65c4de72c16878a1140bbd3986bdd9b8b77e6059
Nadim Akrma <nadimakram759@gmail.com>
2026-01-04 13:38:34 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+
+console.log(3 
+    + 
+    3); // code readability be high
+
+
+let name = "nadim akram"
+let age = 21
+let isLoggedIn = false 
+let state;
+
+
+// number => 2 to power 53
+// biging
+// string =>
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Nadim akram"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
