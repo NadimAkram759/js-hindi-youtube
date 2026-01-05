bae3b14bfe7c7baa7712fd003f5c6c5f11c24361
Nadim Akrma <nadimakram759@gmail.com>
2026-01-05 17:57:13 +0000

    Conversion of datatype

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -31,5 +31,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let sumNumber = 33
 let stringNumber = String(sumNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// **************************** Operations *******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Nadim"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
