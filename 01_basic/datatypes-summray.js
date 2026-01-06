ffc55caae4dd8f5537faba93210757c526f34265
Nadim Akrma <nadimakram759@gmail.com>
2026-01-06 05:52:08 +0000

    datatype summray

---
+++ 01_basic/datatypes-summray.js
@@ -0,0 +1,42 @@
+// primitive
+
+// there are 7 types : string , number , boolean , null , undefine , symbole , bigInt.
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// const bigNumber = 595623296552652161513256n
+
+
+
+
+// Reference (non-primitive)
+
+// Array , functions , objects.
+
+const heros = ["nadim", "naagraj", "doga"]
+let myObj = {
+    name: "nadim",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof anotherId);
+
+
