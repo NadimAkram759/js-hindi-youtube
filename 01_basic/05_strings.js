22fd8b667d66e7c7977fdbf96041b87f1342e46a
Nadim Akrma <nadimakram759@gmail.com>
2026-01-06 08:30:44 +0000

    Strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Nadim"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Nk-nadim-akram')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0, 4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString);
+
+const newStringOne = "  Nadim    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://nadim.com/nadim%20akram"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes("nadim"));
+
+
+console.log(gameName.split('-'));
