44f0a489aad98111819441bebcfb72bbfaf9baca
Nadim Akrma <nadimakram759@gmail.com>
2026-01-28 13:57:18 +0000

    Switch Statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,82 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 12
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("Jun");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("Octuber");
+        break;
+    case 11:
+        console.log("Nomver");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        console.log("Default match")
+        break;
+}
+
+const days = "mon"
+
+switch (days) {
+    case "sun":
+        console.log("Sunday");
+        break;
+    case "mon":
+        console.log("Monday");
+        break;
+    case "tue":
+        console.log("Tuesday");
+        break;
+    case "wed":
+        console.log("Wednesday");
+        break;
+    case "thu":
+        console.log("Thusday");
+        break;
+    case "fri":
+        console.log("Friday");
+        break;
+    case "sat":
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Default day");
+        break;
+}
