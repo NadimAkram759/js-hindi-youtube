5728ecc1c8bc0331b4fc1811b488967eb37a09ce
Nadim Akrma <nadimakram759@gmail.com>
2026-02-03 18:24:16 +0000

    while and dowhile

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash", "batman", "superman", "spiderman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+// do while
+
+// let score = 0
+// do {
+//     console.log(`value of ${score}`);
+//     score = score + 1
+// } while (score < 10);
+
+let score = 11
+do {
+    console.log(`value of ${score}`);
+    score = score + 1
+} while (score < 10);
