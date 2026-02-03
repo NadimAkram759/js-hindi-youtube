ce4f3b8764445f97acf5383f94dc8555095c228e
Nadim Akrma <nadimakram759@gmail.com>
2026-02-03 17:43:42 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,53 @@
+// for loop
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+// console.log(element);
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if(element == 5){
+        // console.log("5 is best number")
+    }
+    // console.log(element);
+}
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and Inner loop ${i}`);
+        // console.log(i + '*' + j + ' = ' + i*j );
+    }
+}
+
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+//   breck and continue
+
+// for (let i = 1; i <= 20; i++) {
+
+//     if (i == 5) {
+//         console.log(`Detected 5`)
+//         break
+//     }
+//     console.log(`Vaue of i is ${i}`);
+// }
+
+for (let i = 1; i <= 20; i++) {
+
+    if (i == 5) {
+        console.log(`Detected 5`)
+        continue
+    }
+    console.log(`Vaue of i is ${i}`);
+}
